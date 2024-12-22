ccefef948ca3284cd5f1e37e069a468f02d85a3a
EvanDaley <daley.evan@gmail.com>
2024-12-21 18:45:51 -0800

    wip

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ cd into project...
 python3 -m venv env
 source env/bin/activate
 pip install --upgrade --quiet langchain
+pip install -qU langchain-openai
+
 ```
 
--- demo.py
+++ demo.py
@@ -1,4 +1,9 @@
-def main():
-    print("Hello, world!");
+import getpass
+import os
 
-main()
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(model="gpt-4o-mini")
